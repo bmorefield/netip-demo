b2b52129053a408ceb96652c8e4fd264a1bc2f38
Bill Morefield <bill@billmorefield.com>
2016-11-11 15:33:37 -0500

    Fixed bug when compiling binaries as auto-launch package wasn’t saved as a dependency.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const ipc = electron.ipcMain
 const Tray = electron.Tray
 const Menu = electron.Menu
 const path = require('path')
-var AutoLaunch = require('auto-launch');
+const AutoLaunch = require('auto-launch');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
